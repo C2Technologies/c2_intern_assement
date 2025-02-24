e3a2735a331b07d523ec8afe046ef2d393f1339a
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-24 17:24:40 +0200

    Implement initial task fetching functionality
    - Add statee management for todo tasks
    - Set up API call to fetch tasks on component mount
    - Add error handling for failed API requests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import getAllTasks from "./services/tasks";
 
-function App() {
+const App = () => {
+  const [todoTasks, setTodoTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchToDoTasks = async () => {
+      try {
+        const tasks = await getAllTasks();
+        setTodoTasks(tasks);
+      } catch (error) {
+        console.error("Error fetching todo tasks:", error)
+      }
+    };
+    fetchToDoTasks();
+  }, []);
+
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    <h2>Tasks</h2>
+    {console.log(todoTasks)}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
