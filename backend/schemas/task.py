4d82d29bf2cc44211236c0b2e7838e3dac83a691
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-25 12:53:16 +0200

    Rename `Task`  class to `TaskBase` and change completed parameter to `False`

--- backend/schemas/task.py
+++ backend/schemas/task.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
-class Task(BaseModel):
+class TaskBase(BaseModel):
     title: str
     description: str
-    completed: Optional[bool] = None
+    completed: Optional[bool] = False
+    
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(TaskBase):
+    title: Optional[str] = None
+
+class Task(TaskBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime]
+    
+    class Config:
+        from_attributes = True
