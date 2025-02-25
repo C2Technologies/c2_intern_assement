5a03dd6920f9044c79cfd352a2a3a4978c94f489
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-25 12:47:07 +0200

    Create alembic configuration and generate migration file

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://to-do%20task_owner:npg_JXrjV0G1MBsH@ep-misty-frost-abz5t8o0-pooler.eu-west-2.aws.neon.tech/to-do%20task?sslmode=require"
+SQLALCHEMY_DATABASE_URL = "postgresql://tasks_owner:npg_MG9cmKCa6YNS@ep-old-base-abw2lxax-pooler.eu-west-2.aws.neon.tech/tasks?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
