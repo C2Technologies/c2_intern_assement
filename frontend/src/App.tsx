e44aa01efa67790dc5510e346dd818bb6202bce8
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-26 12:42:50 +0200

    Add Task Form and TypeScript typings
    - Render Form component for task creation
    - Created TS interface for Task
    - Add state for title and content
    - Implement placeholder `handleTaskSubmit` function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import getAllTasks from "./services/tasks";
+import { Form } from "./components/Form";
+
+interface Task {
+  id: number;
+  title: string;
+  content: string;
+  completed: boolean;
+  createdAt: string;
+  updatedAt: string | null;
+}
 
 const App = () => {
-  const [todoTasks, setTodoTasks] = useState([]);
+  const [todoTasks, setTodoTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
 
   useEffect(() => {
     const fetchToDoTasks = async () => {
@@ -10,16 +22,38 @@ const App = () => {
         const tasks = await getAllTasks();
         setTodoTasks(tasks);
       } catch (error) {
-        console.error("Error fetching todo tasks:", error)
+        console.error("Error fetching todo tasks:", error);
       }
     };
     fetchToDoTasks();
   }, []);
 
+  // const resetInputFields = () => {
+  //   setTitle("");
+  //   setTitle("");
+  // };
+
+  
+  const handleTaskSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    return null;
+  };
+
+
+
+
   return (
     <>
-    <h2>Tasks</h2>
-    {console.log(todoTasks)}
+      <h2>Tasks</h2>
+      <Form
+        title={title}
+        content={content}
+        handleTitleChange={({ target }) => setTitle(target.value)}
+        handleContentChange={({ target }) => setContent(target.value)}
+        handleSubmit={handleTaskSubmit}
+      />
+      {console.log(todoTasks)}
     </>
   );
 };
