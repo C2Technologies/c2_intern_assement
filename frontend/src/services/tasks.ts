5e40ac4eb6673c65cdb71214633383773134f9a4
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-26 13:31:28 +0200

    Create post method for task creation

--- frontend/src/services/tasks.ts
+++ frontend/src/services/tasks.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
+import { TaskCreate } from "../types/types";
 
-const baseUrl = "http://127.0.0.1:8000";
+const baseUrl = "http://127.0.0.1:8000/tasks";
+
 
 const getAllTasks = async () => {
-  const response = await axios.get(`${baseUrl}/tasks`);
+  const response = await axios.get(baseUrl);
   return response.data;
 };
 
-export default getAllTasks;
+const createTask = async (object: TaskCreate) => {
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
+
+export { getAllTasks, createTask};
