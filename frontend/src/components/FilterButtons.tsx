368fa89ef9436b0021bceefeb01321d441c16877
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-28 14:23:57 +0200

    Add FilterButtons component for task filtering

---
+++ frontend/src/components/FilterButtons.tsx
@@ -0,0 +1,51 @@
+type FilterType = "ALL" | "COMPLETED" | "PENDING";
+
+interface FilterButtonsProps {
+  currentFilter: FilterType;
+  onFilterChange: (filter: FilterType) => void;
+}
+
+const FilterButtons = ({
+  currentFilter,
+  onFilterChange,
+}: FilterButtonsProps) => {
+  return (
+    <div className="filter-container">
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="filter"
+            checked={currentFilter === "ALL"}
+            onChange={() => onFilterChange("ALL")}
+          />{" "}
+          All
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="filter"
+            checked={currentFilter === "COMPLETED"}
+            onChange={() => onFilterChange("COMPLETED")}
+          />{" "}
+          Completed
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="filter"
+            checked={currentFilter === "PENDING"}
+            onChange={() => onFilterChange("PENDING")}
+          />{" "}
+          All
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export { FilterButtons };
