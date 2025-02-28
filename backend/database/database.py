debb70edc10f7d1f0805a54d83b521a6e1df452b
Atlegang Sethono <atlegangcode@gmail.com>
2025-02-28 10:32:50 +0200

    Configure database to use environment variable captured in the `config` module

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://tasks_owner:npg_MG9cmKCa6YNS@ep-old-base-abw2lxax-pooler.eu-west-2.aws.neon.tech/tasks?sslmode=require"
+from config import settings
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URL.unicode_string()), connect_args={}, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 Base = declarative_base()
 
