a11a81bf35fc22ff150d986d4499c4cca13dafd5
Atlegang Sethono <atlegangcode@gmail.com>
2025-03-01 17:48:35 +0200

    Extract prop types into TaskListProps interface

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,30 +1,38 @@
 import { Task } from "../types/types";
 import { TaskItem } from "./TaskItem";
 
+interface TaskListProps {
+  tasks: Task[];
+  handleStatusChange: (taskIdL: number, completed: boolean) => void;
+  handleTaskDeletion: (taskId: number, taskTitle: string) => void;
+  handleTaskEdit: (task: Task) => void;
+}
+
 const TaskList = ({
   tasks,
   handleStatusChange,
   handleTaskDeletion,
   handleTaskEdit,
-}: {
-  tasks: Task[];
-  handleStatusChange: (taskIdL: number, completed: boolean) => void;
-  handleTaskDeletion: (taskId: number, taskTitle: string) => void;
-  handleTaskEdit: (task: Task) => void;
-}) => {
+}: TaskListProps) => {
   return (
     <div className="task-list">
-      {tasks.length === 0 ? <p className="no-tasks">No tasks</p> : tasks.map((task) => (
-        <TaskItem
-          key={task.id}
-          title={task.title}
-          completed={task.completed}
-          description={task.description}
-          handleStatusChange={() => handleStatusChange(task.id, task.completed)}
-          handleTaskDeletion={() => handleTaskDeletion(task.id, task.title)}
-          handleTaskEdit={() => handleTaskEdit(task)}
-        />
-      ))}
+      {tasks.length === 0 ? (
+        <p className="no-tasks">No tasks</p>
+      ) : (
+        tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            title={task.title}
+            completed={task.completed}
+            description={task.description}
+            handleStatusChange={() =>
+              handleStatusChange(task.id, task.completed)
+            }
+            handleTaskDeletion={() => handleTaskDeletion(task.id, task.title)}
+            handleTaskEdit={() => handleTaskEdit(task)}
+          />
+        ))
+      )}
     </div>
   );
 };
