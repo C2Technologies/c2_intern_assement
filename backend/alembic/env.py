261a05ef0f7f555fdf2eafe8931ea2917fe1008d
Atlegang Sethono <atlegangcode@gmail.com>
2025-03-02 13:11:17 +0200

    Remove 'backend' directory from statement

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from database.database import Base
-from backend.database.models import task_model
+from database.models import task_model
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
