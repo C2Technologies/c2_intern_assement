f25485564cda2e8c9da00eb29cdd2bcc4c1ff92a
Atlegang Sethono <atlegangcode@gmail.com>
2025-03-02 16:11:15 +0200

    Remove `TaskDescriptionDialog` component and render description in task item
    - Rename state variables to `expanded` and `setExpanded`
    - Restructure Task Item with new div
    - Add conditional rendering for when task has no description

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { TaskDescriptionDialog } from "./TaskDescriptionDialog";
 
 interface TaskDetails {
   title: string;
@@ -18,48 +17,53 @@ const TaskItem = ({
   handleTaskDeletion,
   handleTaskEdit,
 }: TaskDetails) => {
-  const [showDialog, setShowDialog] = useState<boolean>(false);
+  const [expanded, setExpanded] = useState<boolean>(false);
 
-  const toggleDialog = () => {
-    setShowDialog(!showDialog);
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
   };
 
   return (
     <div className="task-container">
-      <div className="task-left">
-        <label className="custom-checkbox">
-          <input
-            type="checkbox"
-            name="completionStatus"
-            checked={completed}
-            onChange={handleStatusChange}
-          />
-          <span></span>
-        </label>
-        <p className="task-name">{title}</p>
+      <div className="task-content">
+        <div className="task-left">
+          <label className="custom-checkbox">
+            <input
+              type="checkbox"
+              name="completionStatus"
+              checked={completed}
+              onChange={handleStatusChange}
+            />
+            <span></span>
+          </label>
+          <p className="task-name">{title}</p>
+        </div>
+        <div className="task-right">
+          {description && (
+            <button onClick={toggleExpanded} className="expand-btn">
+              <img
+                src={`public/images/${
+                  expanded ? "chevron-up" : "chevron-down"
+                }.svg`}
+                alt={expanded ? "collapse" : "expand"}
+              />
+            </button>
+          )}
+          <button onClick={handleTaskEdit} className="edit-btn">
+            <img src="public/images/pen.svg" alt="edit icon" />
+          </button>
+          <button onClick={handleTaskDeletion} className="delete-btn">
+            <img
+              src="public/images/icons8-trash-1-dark.svg"
+              alt="delete icon"
+            />
+          </button>
+        </div>
       </div>
-      <div className="task-right">
-        <button onClick={toggleDialog} className="expand-btn">
-          <img
-            src={`public/images/${
-              showDialog ? "chevron-up" : "chevron-down"
-            }.svg`}
-            alt={showDialog ? "collapse" : "expand"}
-          />
-        </button>
-        <button onClick={handleTaskEdit} className="edit-btn">
-          <img src="public/images/pen.svg" alt="edit icon" />
-        </button>
-        <button onClick={handleTaskDeletion} className="delete-btn">
-          <img src="public/images/icons8-trash-1-dark.svg" alt="delete icon" />
-        </button>
-      </div>
-      {showDialog && description && (
-        <TaskDescriptionDialog
-          title={title}
-          description={description}
-          onClose={toggleDialog}
-        />
+      {expanded && description && (
+        <div className="task-description">
+          <p>{description}</p>
+        </div>
       )}
     </div>
   );
